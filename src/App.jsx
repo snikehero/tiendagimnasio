03baa590a05f6aa5b66d5bc53f6572b43d58c3a2
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2025-06-27 01:56:55 -0600

    Added newProduct component
    Added New Product Component and New Product styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
-
+import NewProduct from "./components/newProduct.jsx";
 function App() {
   return (
     <div>
-      <h1>Pagina del Gimnasio</h1>
+      <h1> Bienvenido a la madriguera</h1>
+      <NewProduct
+        itemName="Nike Metcon 9"
+        itemPrice={29.99}
+        itemImage="public/item_shoes_metcon9.png"
+      />
+      <NewProduct
+        itemName="Playera Negra"
+        itemPrice={29.99}
+        itemImage="public/item_tshirt_playeranegra.png"
+      />
+      <NewProduct
+        itemName="Cbum Protein Powder"
+        itemPrice={29.99}
+        itemImage="public/item_protein_cbum.webp"
+      />
     </div>
   );
 }
