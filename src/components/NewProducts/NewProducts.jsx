03e58bed9f4b11997ba22f2f57908cb15b332f33
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2025-06-27 03:41:55 -0600

    Updated images routes

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -9,21 +9,21 @@ function NewProducts() {
           <Product
             itemName="Nike Metcon 9"
             itemPrice={29.99}
-            itemImage="public/item_shoes_metcon9.png"
+            itemImage="item_shoes_metcon9.png"
           />
         </div>
         <div className="NewProductsColumn">
           <Product
             itemName="Playera Negra"
             itemPrice={29.99}
-            itemImage="public/item_tshirt_playeranegra.png"
+            itemImage="item_tshirt_playeranegra.png"
           />
         </div>
         <div className="NewProductsColumn">
           <Product
             itemName="Cbum Protein"
             itemPrice={29.99}
-            itemImage="public/item_protein_cbum.webp"
+            itemImage="item_protein_cbum.webp"
           />
         </div>
       </div>
