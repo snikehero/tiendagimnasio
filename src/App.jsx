cabee6a41691148a4aba45f02bb522b2e06f21be
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2025-06-27 21:10:28 -0600

    Fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import NavBar from "./components/NavBar/NavBar.jsx";
-import Footer from "./components/Footer/Footer.jsx";
 import MainPage from "./components/MainPage/MainPage.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage.jsx";
 import About from "./components/AboutPage/About.jsx";
 import SingleProductPage from "./components/SingleProductPage/SingleProductPage.jsx";
 import AllProducts from "./components/AllProductsPage/AllProducts.jsx";
-import Home from "./components/Home.jsx";
 import SharedLayout from "./components/SharedLayout/SharedLayout.jsx";
 function App() {
   return (
